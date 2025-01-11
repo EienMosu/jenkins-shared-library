5c05897b354ef1f3ae4b3f931a25534eada43e20
EienMosu <voodoochild9293@hotmail.com>
2025-01-11 19:33:01 +0000

    jenkins fil update with shared librarysssss

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: "dockerhub",passwordVariable: "PASS", usernameVariable: "USER")]) {
-        sh "docker build -t ozkan1234/demo-app:jma-2.0 ."
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push ozkan1234/demo-app:jma-2.0"
+        sh "docker push $imageName"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo "building the application..."
+    echo "building the application for branc $BRANCH_NAME"
     sh "mvn package"
 }
