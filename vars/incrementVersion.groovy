451b12191bcb19a77e0e878f88776ad0ff26697e
EienMosu <voodoochild9293@hotmail.com>
2025-01-12 19:05:50 +0000

    auto versioning

---
+++ vars/incrementVersion.groovy
@@ -0,0 +1,8 @@
+#!/user/bin/env groovy
+
+def call() {
+    echo "incrementing app version..."
+    sh "mvn build-helper:parse-version versions:set \
+-DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+versions:commit"
+}
