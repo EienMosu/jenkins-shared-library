0f4a6aa884a856fe97f7c76874ee5ae41ec2a71d
EienMosu <voodoochild9293@hotmail.com>
2025-01-12 20:16:48 +0000

    git conf is addedsssdss

--- vars/commitVersionUpdate.groovy
+++ vars/commitVersionUpdate.groovy
@@ -1,7 +1,7 @@
 #!/user/bin/env groovy
 
 def call() {
-    withCredentials([usernamePassword(credentialsId: "github-username", passwordVariable: "PASS", usernameVariable: "USER")]) {
+    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_APP_TOKEN')]) {
         sh 'git config --global user.email "jenkins@example.com"'
         sh 'git config --global user.name "jenkins"'
 
@@ -11,7 +11,7 @@ def call() {
 
         echo "------------------------------------------------------------------------------------------"
 
-        sh "git remote set-url origin https://${USER}:${PASS}@github.com/EienMosu/java-maven-app-nana.git"
+        sh "git remote set-url origin https://${GITHUB_APP_TOKEN}@github.com/EienMosu/java-maven-app-nana.git"
         sh "git add *"
         sh 'git commit -m "jenkins version bump"'
         sh "git push origin HEAD:main"
