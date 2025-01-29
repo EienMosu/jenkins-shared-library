d62c6274d568f61849feaeff2823aaf1e14c6d71
EienMosu <voodoochild9293@hotmail.com>
2025-01-29 07:32:37 +0000

    github login fix

--- vars/commitVersionUpdate.groovy
+++ vars/commitVersionUpdate.groovy
@@ -1,7 +1,7 @@
 #!/user/bin/env groovy
 
 def call() {
-    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_APP_TOKEN')]) {
+    withCredentials([usernamePassword(credentialsId: 'github-token', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_APP_TOKEN')]) {
         sh 'git config --global user.email "jenkins@example.com"'
         sh 'git config --global user.name "jenkins"'
 
@@ -12,8 +12,8 @@ def call() {
         echo "------------------------------------------------------------------------------------------"
 
         sh "git remote set-url origin https://${GITHUB_APP_TOKEN}@github.com/EienMosu/java-maven-app-nana.git"
-        sh "git add *"
-        sh 'git commit -m "jenkins version bump"'
+        sh "git add ."
+        sh 'git commit -m "jenkins version bump" || echo "No changes to commit"'
         sh "git push origin HEAD:main"
     }
 }
